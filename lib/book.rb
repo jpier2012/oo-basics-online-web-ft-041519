3dbbca5a896b24aa48ae45b3aca2482a2370278e
James Pier <jpier2012@gmail.com>
2019-04-16 16:57:20 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,11 @@
 class Book
+  def initialize(title, author, page_count, genre)
+    @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
+  end
+  
   attr_accessor :title, :author, :page_count, :genre
 
   def turn_page
